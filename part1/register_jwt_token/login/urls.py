2d1bd90a80010e445eeef63f3a355ab39a5b8885
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-11-25 23:06:34 +0500

    fix register_JWT

--- part1/register_jwt_token/login/urls.py
+++ part1/register_jwt_token/login/urls.py
@@ -1,2 +1,8 @@
 # TODO напишите здесь url для получения токена
-urlpatterns = []
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+]
