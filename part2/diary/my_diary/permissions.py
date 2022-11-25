c8ed8b87c2becd6e723a8cfbb676c6c3ffd54955
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-11-26 01:17:43 +0500

    fix my_diary

--- part2/diary/my_diary/permissions.py
+++ part2/diary/my_diary/permissions.py
@@ -1,6 +1,14 @@
+from django.contrib.auth import get_user_model
 from rest_framework import permissions
+
+User = get_user_model()
 
 
 # TODO напишите здесь необходимые условия в соответствии с заданием
 class MarkCreatePermission(permissions.BasePermission):
     message = 'Adding marks for non teacher user not allowed.'
+
+    def has_permission(self, request, view):
+        if request.user.role != User.TEACHER:
+            return False
+        return True
--- part2/diary/my_diary/views.py
+++ part2/diary/my_diary/views.py
@@ -2,10 +2,13 @@ from rest_framework.generics import CreateAPIView
 
 from my_diary.models import Mark
 from my_diary.serializers import MarkCreateSerializer
+from rest_framework.permissions import IsAuthenticated
+from my_diary.permissions import MarkCreatePermission
 
 
 # TODO доработайте класс ниже в соответствии с условиями задания
 class MarkCreateView(CreateAPIView):
     queryset = Mark.objects.all()
     serializer_class = MarkCreateSerializer
+    permission_classes = [IsAuthenticated, MarkCreatePermission]
 
