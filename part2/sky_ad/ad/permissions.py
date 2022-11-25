44dcb8a3781d87d14ec478f6a849d0ec483df435
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-11-26 01:25:21 +0500

    fix sky_ad

--- part2/sky_ad/ad/permissions.py
+++ part2/sky_ad/ad/permissions.py
@@ -4,3 +4,12 @@ from rest_framework import permissions
 # TODO напишите здесь необходимые условия в соответствии с заданием
 class AdUpdatePermission(permissions.BasePermission):
     message = 'Updating not your add is not permitted'
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if hasattr(obj, "user"):
+            return request.user and request.user.is_authenticated and obj.user == request.user
+        else:
+            return request.user and request.user.is_authenticated and obj.user == request.user
--- part2/sky_ad/ad/views.py
+++ part2/sky_ad/ad/views.py
@@ -3,8 +3,11 @@ from rest_framework.generics import UpdateAPIView
 from ad.models import Ad
 from ad.serializers import AdSerializer
 
+from solution.part2.sky_ad.ad.permissions import IsOwner
+
 
 # TODO добавьте ниже код в соответствии с заданием
 class AdUpdateView(UpdateAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
+    permission_classes = [IsOwner]
