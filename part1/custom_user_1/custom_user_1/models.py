5cc284cdd7dd13363b2880a8c7e4c4dbfca6c349
DanilMirosh <danil@iMac-DanilMirosh.local>
2022-11-25 15:54:12 +0500

    fix custom_1

--- part1/custom_user_1/custom_user_1/models.py
+++ part1/custom_user_1/custom_user_1/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # TODO Опишите модель профиль ниже
 # TODO Профиль пользователя должен иметь следующие поля
 # адрес (address) длинной не более 50 символов
 # телефон (phone) - номер должен храниться в формате
-class Profile:
-    pass
+class Profile(User):
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=12)
+
