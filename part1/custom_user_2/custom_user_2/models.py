0e743a758492bffb91ca28ee98dc6cde6ab131b4
DanilMirosh <danil@iMac-DanilMirosh.local>
2022-11-25 16:08:04 +0500

    fix custom_2

--- part1/custom_user_2/custom_user_2/models.py
+++ part1/custom_user_2/custom_user_2/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 # TODO опишите модель кастомного пользователя ниже
 class CustomUser(AbstractUser):
-    pass
+    TEACHER = "teacher"
+    STUDENT = "student"
+    ROLE = [(TEACHER, TEACHER), (STUDENT, STUDENT)]
+
+    birth_date = models.DateField()
+    role = models.CharField(max_length=7, choices=ROLE)
+
+    def save(self, *args, **kwargs):
+        self.set_password(self.password)
+
+        super().save()
